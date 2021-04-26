e0a1fe4c49fde7abca0d490bd3f16ef976f781df
Daniel Raymond <dan@raymond.ch>
2021-04-26 12:02:32 -0400

    Fixed TS error

--- components/LineCurve.vue
+++ components/LineCurve.vue
@@ -4,33 +4,32 @@
 	</div>
 </template>
 
-
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-	name: "Line",
+	name: "LineCurve",
 	methods: {
-		heroLine(ctx) {
+		heroLine(ctx: CanvasRenderingContext2D) {
 			ctx.moveTo(200, 0);
 			ctx.lineTo(200, 700);
 			ctx.stroke();
 		},
-		heroAboutLine(ctx) {
+		heroAboutLine(ctx: CanvasRenderingContext2D) {
 
 		}
 	},
 	mounted() {
-		const c = this.$refs.canvas;
+		const c = this.$refs.canvas as HTMLCanvasElement;
 		const ctx = c.getContext("2d");
-		ctx.strokeStyle = 'hsl(16, 95%, 65%)';
-		ctx.lineWidth = 8;
-		this.heroLine(ctx);
+		if (ctx) {
+			ctx.strokeStyle = 'hsl(16, 95%, 65%)';
+			ctx.lineWidth = 8;
+			this.heroLine(ctx);
+			this.heroAboutLine(ctx);
+		}
 	}
 })
 </script>
 
 <style scoped>
-/* .line-wrapper {
-	background: grey;
-} */
 </style>
