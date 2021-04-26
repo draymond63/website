da1e2cf9d53916803ca58a1416d5edd889221249
Daniel Raymond <dan@raymond.ch>
2021-04-26 15:36:51 -0400

    Added line & arc drawing funcs

--- components/LineCurve.vue
+++ components/LineCurve.vue
@@ -1,7 +1,15 @@
 <template>
 	<div>
 		<svg ref="line">
-			<circle cx="50" cy="50" r="40" stroke="green" stroke-width="8" fill="yellow" />
+			<!-- <path d="M115 0V23.5C115 51 137 74 165 73.5H604.5C632 74 655 96 655 123.5V418C654 446 632 468 605 468H54C26 468 4 490 4 518V548"/> -->
+			<path :d="path"/>
+			<!-- "
+				M 200 0 
+				l 0 500
+				a 50,50 0 0,0 50,50
+				l 500 0
+				a 50,50 0 0,1 50,50
+				" -->
 		</svg>
 	</div>
 </template>
@@ -12,9 +20,8 @@ export default Vue.extend({
 	name: "LineCurve",
 	data() {
 		return {
-			points: {
-				
-			}
+			radius: 50,
+			path: "M 200 0\n"
 		}
 	},
 	methods: {
@@ -23,20 +30,69 @@ export default Vue.extend({
 			const svg = this.$refs.line as SVGSVGElement;
 			if (svg.parentElement) {
 				const width = svg.parentElement.offsetWidth;
-				const height = svg.parentElement.offsetHeight;
+				const height = svg.parentElement.offsetHeight + 1000;
 				// Set svg attributes
 				const vb = [0, 0, width,height];
 				svg.setAttribute("viewBox", vb.join(" ") );
 				svg.setAttribute("width", width.toString() );
 				svg.setAttribute("height", height.toString() );
 			}
+		},
+		// * Line Functions
+		addSegment(segment: String) {
+			this.path += segment + '\n';
+		},
+		line(length: number, {vert = true}) {
+			if (vert)
+				this.addSegment(`l 0 ${length}`);
+			else
+				this.addSegment(`l ${length} 0`);
+		},
+		arc({top = true, right = true}) {
+			const r = this.radius;
+			this.addSegment(`a ${r},${r} 0 0,${right?1:0} ${top === right?'':'-'}${r}, ${r}`);
+			// if (top && right) {
+			// 	this.addSegment(`a ${r},${r} 0 0,1 ${r},${r}`);
+			// } else if (!top && right) {
+			// 	this.addSegment(`a ${r},${r} 0 0,1 -${r},${r}`);
+			// } else if (top && !right) {
+			// 	this.addSegment(`a ${r},${r} 0 0,0 -${r},${r}`);
+			// } else if (!top && !right) {
+			// 	this.addSegment(`a ${r},${r} 0 0,0 ${r},${r}`);
+			// }
+		},
+
+		// * Line Creation
+		heroSection() {
+			this.line(500, {vert: true});
+			this.arc({top: false, right: false});
+			this.line(500, {vert: false});
+			this.arc({top: true, right: true});
+			this.line(100, {vert: true});
+			this.arc({top: false, right: true});
+			this.line(-500, {vert: false});
+			this.arc({top: true, right: false});
+		},
+		initLine() {
+			this.heroSection();
 		}
 	},
+
 	mounted() {
 		this.setBoundaries();
 		// Resize bounding box
 		// ! Necessary only if dimensions are dependent on width of svg, not on imported content widths
 		window.addEventListener('resize', () => this.setBoundaries());
+		// Draw line
+		this.initLine();
 	}
 })
 </script>
+
+<style lang="postcss" scoped>
+path {
+	stroke: var(--main-color-1);
+	stroke-width: 8px;
+	fill: none;
+}
+</style>
