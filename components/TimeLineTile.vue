339aa01d3381e7ff7764debc1bbc8d99e038b400
Daniel Raymond <dan@raymond.ch>
2021-04-29 02:27:29 -0400

    Styled timeline

---
+++ components/TimeLineTile.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="tile">
+		
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+	name: 'Tile',
+})
+</script>
+
+<style lang="postcss" scoped>
+.tile {
+	width: 100%;
+	height: 10rem;
+	background: #f5f5f5;
+	box-shadow: 0px 4px 8px 0px #e4785140;
+}
+</style>
--- components/Timeline.vue
+++ components/Timeline.vue
@@ -1,41 +1,69 @@
 <template>
 	<div id="timeline">
 		<h2>A Timeline</h2>
-		<h3>Ha projects are fun</h3>
+		<h3>A brief overview of what I've done</h3>
+		<div>
+			<button>Projects</button>
+			<button>Experience</button>
+		</div>
 
-		<div class="project-grid">
-			<div class="proj"></div>
-			<div class="proj"></div>
-			<div class="proj"></div>
+		<div class="grid">
+			<tile/>
+			<div class="dot"></div>
+			<tile/>
+			<tile/>
+			<div class="dot"></div>
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
+import data from "@/assets/timeline.json";
+import tile from "@/components/TimeLineTile.vue";
+
 import Vue from 'vue'
 export default Vue.extend({
 	name: 'Projects',
+	components: {
+		tile
+	},
+	data() {
+		return {
+			info: data
+		}
+	}
 })
 </script>
 
 <style lang="postcss" scoped>
-#projects {
+#timeline {
 	margin: 0 5rem;
 	padding: 3rem;
+	--dot-size: 2rem;
 }
 
-.project-grid {
+.grid {
 	display: grid;
-	grid-template-columns: 1fr 1fr;
+	grid-template-columns: 1fr var(--dot-size) 1fr;
+	place-items: center;
 	gap: 1rem 2rem;
 }
 
-.proj {
-	width: 100%;
-	height: 10rem;
-	background: grey;
+.dot {
+	width: var(--dot-size);
+	height: var(--dot-size);
+	border-radius: 50%;
+	background: #f9f5f2;
+	border: .3rem solid #fb7f53
 }
 
+button {
+	background: #ECAD5F33;
+	border: 1px solid #ECAD5F;
+	border-radius: 1rem;
+	margin: .5rem;
+	margin-left: 0;
+}
 
 @media screen and (max-width: 850px) {
 	#projects {
